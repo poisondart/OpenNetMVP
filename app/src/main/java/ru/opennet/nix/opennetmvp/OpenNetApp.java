0b4924d9d597291c5fd611b2731a065591ac2c59
Nikita K <novembernix@gmail.com>
2018-08-04 20:17:34 +0300

    Активити статьи и комментариев закрываются свайпом влево

--- app/src/main/java/ru/opennet/nix/opennetmvp/OpenNetApp.java
+++ app/src/main/java/ru/opennet/nix/opennetmvp/OpenNetApp.java
@@ -1,6 +1,9 @@
 package ru.opennet.nix.opennetmvp;
 
 import android.app.Application;
+
+import com.gw.swipeback.tools.WxSwipeBackActivityManager;
+
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 
@@ -8,6 +11,7 @@ public class OpenNetApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        WxSwipeBackActivityManager.getInstance().init(this);
         Realm.init(getApplicationContext());
         RealmConfiguration realmConfig = new RealmConfiguration.Builder()
                 .name("opennet.realm")
